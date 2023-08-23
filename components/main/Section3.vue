1daebda8b1e0e74bcd083988b9d32df000f9789d
Tony <qkrdkwl9090@naver.com>
2023-08-23 23:47:17 +0900

    feat(main): section3

---
+++ components/main/Section3.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+type Item = {
+  title: string
+  path: string
+  image: string
+}
+const list: Array<Item> = [
+  {
+    title: 'GSAP Animation 1',
+    path: '/gsap/1',
+    image: '/portfoilo/img/main/1.webp',
+  },
+  {
+    title: 'GROW',
+    path: '/gsap/2',
+    image: '/portfoilo/img/main/2.webp',
+  },
+]
+</script>
+<template>
+  <section class="flex flex-col gap-[.625rem]">
+    <div
+      v-for="item of list"
+      :key="item.title"
+      class="w-screen relative [&>a>p]:hover:opacity-100"
+    >
+      <NuxtLink :to="item.path">
+        <img
+          :src="item.image"
+          alt=""
+          class="w-full md:h-[450px] object-cover object-top"
+        />
+        <p
+          class="absolute top-12 right-12 text-white text-md md:text-2xl font-bold opacity-0 transition-opacity duration-300"
+        >
+          {{ item.title }}
+        </p>
+      </NuxtLink>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <section class="main__container w-full min-h-screen">
+    <!-- Top Visual -->
     <MainSection1 />
+    <!-- Intro -->
     <MainSection2 />
+    <!-- Portfolio List -->
+    <MainSection3 />
   </section>
 </template>
