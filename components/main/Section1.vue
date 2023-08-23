e4a1da1edbb8a9ce882f9fb7d6516fcf8669ed71
Tony <qkrdkwl9090@naver.com>
2023-08-23 23:05:59 +0900

    feat(main): section1

---
+++ components/main/Section1.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="w-screen h-screen relative">
+    <video
+      class="w-full h-full object-cover absolute top-0 left-0 -z-10"
+      src="/video/main_title-bg.mp4"
+      data-autoplay
+      autoplay
+      loop
+      muted
+    ></video>
+    <div
+      class="px-6 md:px-8 md:mx-auto h-full md:w-[87.5rem] flex items-end text-white"
+    >
+      <div class="mb-32">
+        <p class="text-2xl md:text-5xl font-bold">Welcome to My Portfolio</p>
+        <p class="text-md md:text-2xl mt-2">
+          I'm going to make something that I want to make
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
