c7e41ce649236cb4549fb4a632bf1fab1485efb3
Tony <qkrdkwl9090@naver.com>
2023-08-23 23:26:16 +0900

    feat(main): section2

---
+++ components/main/Section2.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="w-screen py-16 md:py-32">
+    <div
+      class="px-6 md:px-8 md:mx-auto md:max-w-[87.5rem] flex flex-col md:flex-row md:justify-between gap-10"
+    >
+      <div class="md:w-1/5 text-xl">
+        <p class="font-bold text-gray-400">Skills</p>
+        <p class="mt-4">
+          HTML, CSS, Javascrdipt, Typescript, Tailwind CSS, Styled-Component,
+          SCSS, Webpack, Vite, Vue, Nuxt
+        </p>
+      </div>
+      <div class="md:w-1/5">
+        <p class="font-bold text-gray-400 text-xl">Contack</p>
+        <ul class="mt-4 text-gray-500 text-md flex flex-col">
+          <li>
+            <NuxtLink href="https://github.com/qkrdkwl9090" target="_blank"
+              >Github</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink href="mailto:qkrdkwl9090@naver.com">Email</NuxtLink>
+          </li>
+        </ul>
+      </div>
+      <div class="md:w-1/5">
+        <p class="text-xl font-bold text-gray-400">Intro</p>
+        <p class="mt-4 text-md">
+          안녕하세요. 3년 차 프론트 개발자 박동현입니다. 꾸준함과 열정을
+          장점으로, 배운 것을 기록하는 습관을 가지고 있습니다. 조금씩 발전하는
+          사람이 되기 위해 노력합니다.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
